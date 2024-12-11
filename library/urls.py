45104e58b06052a7e3e4b4bc5867a8a7119d234f
James-Trema <andrdu29@live.fr>
2024-12-11 09:44:54 +0100

    up

--- library/urls.py
+++ library/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path('authors/<int:author_id>/', views.author_detail),
     path('authors/', views.author_list, name='authors_list'),
-    path('books/', views.books_list)
+    path('books/', views.books_list, name='books_list')
 ]
