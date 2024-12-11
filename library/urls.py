502a8b135a8744d86cf37f54ca326b0b7a25febc
James-Trema <andrdu29@live.fr>
2024-12-11 14:32:08 +0100

    up

--- library/urls.py
+++ library/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path('authors/<int:author_id>/', views.author_detail),
     path('authors/alone/<int:pk>/', views.AuthorDetail.as_view()),
     path('authors/', views.AuthorList.as_view()),
-    path('books/', views.BooksView.as_view()),
+    path('books/', views.BooksView.as_view(), name="books"),
     path('books/<int:pk>/delete', views.BooksView.as_view(), name="delete_book")
 ]
