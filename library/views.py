ee657585f1f01cd99efd67180653c28cf3853266
James-Trema <andrdu29@live.fr>
2024-12-11 14:30:24 +0100

    up

--- library/views.py
+++ library/views.py
@@ -1,76 +1,81 @@
-from django.http import HttpRequest, HttpResponse, Http404, HttpResponseRedirect, HttpResponseForbidden
+from django.http import HttpRequest, HttpResponse, Http404, HttpResponseRedirect
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.decorators import login_required
 from django.views.generic import View, DetailView
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib import messages
+from django.core.paginator import Paginator
 from .models import Author, Book
 from .forms import AuthorForm, BookForm
 from .mixins import PostPermissionMixin
 
 class AuthorList(LoginRequiredMixin, View):
-    # def dispatch(self, request, *args, **kwargs):
-    #     if request.method == "POST" and not request.user.is_authenticated:
-    #         return HttpResponseForbidden("You are not authorized to perform this action.")
-    #     return super().dispatch(request, *args, **kwargs)
-    
     def get(self, request: HttpRequest) -> HttpResponse:
         form = AuthorForm()
         return render(
             request,
             'authors.html',
-            { 'authors': Author.objects.all(), 'form': form },
+            {'authors': Author.objects.all(), 'form': form},
         )
-    def post(self, request: HttpRequest)->HttpResponse:
+
+    def post(self, request: HttpRequest) -> HttpResponse:
         form = AuthorForm(request.POST)
-        if form.is_valid(): # Boom on valide les champs
-            form.save()     # Boom enregistrer en bdd
+        if form.is_valid():
+            form.save()
             return HttpResponseRedirect('')
         messages.error(request, "Erreur dans le formulaire")
         return render(
             request,
             'authors.html',
-            { 'authors': Author.objects.all(), 'form': form },
+            {'authors': Author.objects.all(), 'form': form},
         )
-    
-class BooksView(PostPermissionMixin, View):
 
+class BooksView(PostPermissionMixin, View):
     def dispatch(self, request, *args, **kwargs):
         if request.method == "POST" and '_method' in request.POST:
-            return self.delete(request, *args, **kwargs)
+            pk = request.POST.get('id')
+            if pk:
+                return self.delete(request, pk, *args, **kwargs)
+            else:
+                raise Http404("ID is required to delete a book.")
         return super().dispatch(request, *args, **kwargs)
 
-    def get(self, request: HttpRequest)->HttpResponse:
+    def get(self, request: HttpRequest) -> HttpResponse:
         form = BookForm()
+        books_list = Book.objects.all()
+        paginator = Paginator(books_list, 10)
+        page_number = request.GET.get('page')
+        books = paginator.get_page(page_number)
         return render(
             request,
             'books.html',
-            { 'form': form, 'books': Book.objects.all() },
+            {'form': form, 'books': books},
         )
-    
-    def post(self, request: HttpRequest)->HttpResponse:
+
+    def post(self, request: HttpRequest) -> HttpResponse:
         form = BookForm(request.POST)
         if form.is_valid():
             form.save()
-            form = BookForm()
+            messages.success(request, "Le livre a été ajouté avec succès.")
+            return redirect('books')
+        messages.error(request, "Erreur lors de l'ajout du livre.")
         return render(
             request,
             'books.html',
-            { 'form': form, 'books': Book.objects.all() },
+            {'form': form, 'books': Book.objects.all()},
         )
-    
-    def delete(self, request: HttpRequest, pk: int)->HttpResponse:
-        print('PASS')
+
+    def delete(self, request: HttpRequest, pk: int) -> HttpResponse:
         try:
             book = Book.objects.get(id=pk)
             book.delete()
+            messages.success(request, "Le livre a été supprimé avec succès.")
         except Book.DoesNotExist:
             raise Http404("Book does not exist")
-        return HttpResponseRedirect(redirect_to='/books')
+        return redirect('books')
 
-# Create your views here as functions.
-@login_required # comes from django.contrib.auth.decorators
-def author_detail(request: HttpRequest, author_id: int)->HttpResponse:
+@login_required
+def author_detail(request: HttpRequest, author_id: int) -> HttpResponse:
     try:
         author = Author.objects.get(id=author_id)
     except Author.DoesNotExist:
@@ -78,9 +83,9 @@ def author_detail(request: HttpRequest, author_id: int)->HttpResponse:
     return render(
         request,
         'author_detail.html',
-        { 'author': author },
+        {'author': author},
     )
-    
+
 class AuthorDetail(DetailView):
     model = Author
     template_name = "author_detail.html"
