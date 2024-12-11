867ca0ab57d963bedbcbf5b662054486b2c812d4
James-Trema <andrdu29@live.fr>
2024-12-12 00:19:14 +0100

    up

--- library/views.py
+++ library/views.py
@@ -2,10 +2,9 @@ from django.http import HttpRequest, HttpResponse, Http404, HttpResponseRedirect
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.decorators import login_required
 from django.views.generic import View, DetailView
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib import messages
 from django.core.paginator import Paginator
-from django.shortcuts import get_object_or_404
 from .models import Author, Book
 from .forms import AuthorForm, BookForm
 from .mixins import PostPermissionMixin
@@ -75,31 +74,42 @@ class BooksView(PostPermissionMixin, View):
             raise Http404("Book does not exist")
         return redirect('books')
 
-@login_required
-def author_detail(request: HttpRequest, author_id: int) -> HttpResponse:
-    author = get_object_or_404(Author, id=author_id)
-    if request.method == 'POST':
-        if 'name' in request.POST:  # Formulaire de modification d'auteur
-            author_form = AuthorForm(request.POST, instance=author)
-            if author_form.is_valid():
-                author_form.save()
-                messages.success(request, "Auteur modifié avec succès.")
-        elif 'title' in request.POST:  # Formulaire d'ajout de livre
-            book_form = BookForm(request.POST)
-            if book_form.is_valid():
-                book = book_form.save(commit=False)
-                book.author = author
-                book.save()
-                messages.success(request, "Livre ajouté avec succès.")
-    else:
-        author_form = AuthorForm(instance=author)
-        book_form = BookForm()
+    @login_required
+    def author_detail(request: HttpRequest, author_id: int) -> HttpResponse:
+        author = get_object_or_404(Author, id=author_id)
+        if request.method == 'POST':
+            if 'name' in request.POST:  # Formulaire de modification d'auteur
+                author_form = AuthorForm(request.POST, instance=author)
+                if author_form.is_valid():
+                    author_form.save()
+                    messages.success(request, "Auteur modifié avec succès.")
+            elif 'title' in request.POST:  # Formulaire d'ajout de livre
+                book_form = BookForm(request.POST)
+                if book_form.is_valid():
+                    book = book_form.save(commit=False)
+                    book.author = author
+                    book.save()
+                    messages.success(request, "Livre ajouté avec succès.")
+        else:
+            author_form = AuthorForm(instance=author)
+            book_form = BookForm()
 
-    return render(
-        request,
-        'author_detail.html',
-        {'author': author, 'author_form': author_form, 'book_form': book_form},
-    )
+        return render(
+            request,
+            'author_detail.html',
+            {'author': author, 'author_form': author_form, 'book_form': book_form},
+        )
+
+    def edit_book(request, book_id):
+        book = get_object_or_404(Book, id=book_id)
+        if request.method == "POST":
+            form = BookForm(request.POST, instance=book)
+            if form.is_valid():
+                form.save()
+                return redirect('author_detail', book.author.id)
+        else:
+            form = BookForm(instance=book)
+        return render(request, 'editbook.html', {'form': form})
 
 class AuthorDetail(DetailView):
     model = Author
