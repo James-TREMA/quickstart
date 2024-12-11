2e472e6a81646cb2f7681ec38dea62b2441c505c
James-Trema <andrdu29@live.fr>
2024-12-11 09:47:28 +0100

    up

--- library/urls.py
+++ library/urls.py
@@ -1,8 +1,13 @@
-from django.urls import path
+from django.contrib.auth import views as auth_views
+from django.urls import path, include
+
 from . import views
 
 urlpatterns = [
     path('authors/<int:author_id>/', views.author_detail),
     path('authors/', views.author_list, name='authors_list'),
-    path('books/', views.books_list, name='books_list')
+    path('books/', views.books_list, name='books_list'),
+
+    path('accounts/logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
