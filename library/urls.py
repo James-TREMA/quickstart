ff39ae6006bac5faf5bbde23ca0719f8f71e3a72
James-Trema <andrdu29@live.fr>
2024-12-12 09:09:03 +0100

    up

--- library/urls.py
+++ library/urls.py
@@ -2,14 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # path('authors/<int:author_id>/', views.author_detail),
-    path('authors/alone/<int:pk>/', views.AuthorDetail.as_view()),
-    path('authors/', views.AuthorList.as_view()),
-    path('books/', views.BooksView.as_view(), name="books"),
-    path('books/<int:pk>/delete', views.BooksView.as_view(), name="delete_book"),
-
-
+    path('authors/alone/<int:pk>/', views.AuthorDetail.as_view(), name="author_alone"),
+    path('authors/', views.AuthorList.as_view(), name="author_list"),
     path('authors/<int:author_id>/', views.author_detail, name='author_detail'),
+
+    path('books/', views.BooksView.as_view(), name="books"),
     path('books/<int:book_id>/edit/', views.edit_book, name='edit_book'),
     path('books/<int:book_id>/delete/', views.delete_book, name='delete_book'),
 ]
