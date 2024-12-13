f2e17b487300d03c528c806f7f2bf93b8284c054
James-Trema <andrdu29@live.fr>
2024-12-13 09:25:50 +0100

    up

--- library/urls.py
+++ library/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path('authors/<int:pk>/', views.AuthorDetail.as_view(), name='author_detail'),
 
     path('books/', views.BooksView.as_view(), name="books"),
-    # path('books/<int:book_id>/edit/', views.edit_book, name='edit_book'),
-    # path('books/<int:book_id>/delete/', views.delete_book, name='delete_book'),
+    path('books/<int:book_id>/edit/', views.BooksView.as_view(), name='edit_book'),
+    path('books/<int:book_id>/delete/', views.BooksView.as_view(), name='delete_book'),
 ]
